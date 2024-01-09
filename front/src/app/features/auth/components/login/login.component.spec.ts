575e2d07225b6a6557de0ae04e8b98f089c4c538
Baptiste LABAUNE <baptiste.labaune@gmail.com>
2024-01-09 18:22:11 +0100

    progress on unit test

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -18,7 +18,7 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
 import { LoginRequest } from '../../interfaces/loginRequest.interface';
 
@@ -26,10 +26,14 @@ describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  class RouterMock {
+    navigate = jest.fn();
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [{ provide: Router, useClass: RouterMock }, SessionService],
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
@@ -84,12 +88,10 @@ describe('LoginComponent', () => {
     tick();
     fixture.detectChanges();
     flush();
+    console.log(routerNavigateSpy.mock.calls);
 
     // Check if the navigate function was called with the correct parameters
-    expect(routerNavigateSpy).toHaveBeenCalledWith(
-      ['/sessions'],
-      expect.anything()
-    );
+    expect(routerNavigateSpy).toHaveBeenCalledWith(['/sessions']);
 
     // Ensure that navigate was called only once
     expect(routerNavigateSpy).toHaveBeenCalledTimes(1);
@@ -100,6 +102,55 @@ describe('LoginComponent', () => {
       password: 'password123',
     });
     expect(sessionServiceLogIn).toHaveBeenCalledWith(mockSessionInformation);
+
+    // Clear the mocks
+    authServiceLoginSpy.mockRestore();
+    sessionServiceLogIn.mockRestore();
+    routerNavigateSpy.mockRestore();
+  }));
+
+  it('should handle error when submitting invalid credentials', fakeAsync(() => {
+    // Set form values
+    component.form.patchValue({
+      email: 'test@example.com',
+      password: 'password123',
+    });
+
+    const router = TestBed.inject(Router);
+
+    // Mock the navigate method to intercept without triggering actual navigation
+    const routerNavigateSpy = jest.spyOn(router, 'navigate');
+
+    // Mock the authService login to return an observable that emits an error
+    const authServiceLoginSpy = jest
+      .spyOn(component['authService'], 'login')
+      .mockReturnValue(throwError(() => new Error('Invalid credentials')));
+
+    const sessionServiceLogIn = jest.spyOn(
+      component['sessionService'],
+      'logIn'
+    );
+
+    component.submit();
+    tick();
+    fixture.detectChanges();
+    flush();
+    console.log(routerNavigateSpy.mock.calls);
+
+    // Check if the navigate function was not called
+    expect(routerNavigateSpy).not.toHaveBeenCalled();
+
+    // Assert other expectations
+    expect(authServiceLoginSpy).toHaveBeenCalledWith({
+      email: 'test@example.com',
+      password: 'password123',
+    });
+
+    // Ensure that onError flag is set to true
+    expect(component.onError).toBe(true);
+
+    // Ensure that sessionServiceLogIn was not called
+    expect(sessionServiceLogIn).not.toHaveBeenCalled();
 
     // Clear the mocks
     authServiceLoginSpy.mockRestore();
