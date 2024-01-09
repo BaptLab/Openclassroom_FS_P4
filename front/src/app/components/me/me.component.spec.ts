1c3dd7d02796ca0dbd21910aa9d5ae513ab8fae5
Baptiste LABAUNE <baptiste.labaune@gmail.com>
2024-01-09 16:59:17 +0100

    unit tests on front added

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -1,8 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
+import { EMPTY, Observable } from 'rxjs'; // Import EMPTY from RxJS
+import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import {
   ComponentFixture,
   TestBed,
   fakeAsync,
+  flush,
   tick,
 } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
@@ -11,9 +16,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from 'src/app/services/session.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { MeComponent } from './me.component';
 import { Router } from '@angular/router';
+import { User } from 'src/app/interfaces/user.interface';
+import { of } from 'rxjs';
 
 describe('MeComponent', () => {
   let component: MeComponent;
@@ -24,6 +32,7 @@ describe('MeComponent', () => {
       admin: true,
       id: 1,
     },
+    logOut: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -35,7 +44,9 @@ describe('MeComponent', () => {
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
+        NoopAnimationsModule,
         MatInputModule,
+        RouterTestingModule,
       ],
       providers: [{ provide: SessionService, useValue: mockSessionService }],
     }).compileComponents();
@@ -46,14 +57,68 @@ describe('MeComponent', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy() as any;
+    expect(component).toBeTruthy();
   });
 
-  it('should navigate to browser previous page', fakeAsync(() => {
+  it('should navigate to the previous page', fakeAsync(() => {
     const spy = jest.spyOn(window.history, 'back').mockImplementation(() => {});
     component.back();
     tick();
-    expect(spy).toHaveBeenCalled() as any;
+    expect(spy).toHaveBeenCalled();
     spy.mockRestore();
+  }));
+
+  it('should get the user on init', fakeAsync(() => {
+    const mockUser: User = {
+      id: 1,
+      email: 'john@gmail.com',
+      lastName: 'Doe',
+      firstName: 'John',
+      admin: true,
+      password: 'pwd',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const userServiceGetByIdSpy = jest
+      .spyOn(component['userService'], 'getById')
+      .mockReturnValue(of(mockUser));
+
+    component.ngOnInit();
+    tick();
+    fixture.detectChanges();
+
+    expect(userServiceGetByIdSpy).toHaveBeenCalled();
+    expect(component.user).toEqual(mockUser);
+
+    userServiceGetByIdSpy.mockRestore();
+  }));
+
+  it('should delete the user', fakeAsync(() => {
+    const routerNavigateSpy = jest.spyOn(TestBed.inject(Router), 'navigate');
+    const userServiceDeleteSpy = jest
+      .spyOn(component['userService'], 'delete')
+      .mockImplementation((id: string) => of(null));
+    const matSnackBarOpenSpy = jest.spyOn(component['matSnackBar'], 'open');
+
+    component.delete();
+    tick();
+    fixture.detectChanges();
+    flush();
+
+    expect(userServiceDeleteSpy).toHaveBeenCalledWith('1');
+    expect(matSnackBarOpenSpy).toHaveBeenCalledWith(
+      'Your account has been deleted !',
+      'Close',
+      {
+        duration: 3000,
+      }
+    );
+    expect(component['sessionService'].logOut).toHaveBeenCalled();
+    expect(routerNavigateSpy).toHaveBeenCalledWith(['/']);
+
+    userServiceDeleteSpy.mockRestore();
+    matSnackBarOpenSpy.mockRestore();
+    routerNavigateSpy.mockRestore();
   }));
 });
--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -1,5 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  flush,
+  tick,
+} from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -7,10 +13,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { Observable, of } from 'rxjs';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
+import { LoginRequest } from '../../interfaces/loginRequest.interface';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -39,4 +49,61 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should submit credentials and navigate to the session when valid', fakeAsync(() => {
+    const mockSessionInformation: SessionInformation = {
+      token: 'your_mocked_token',
+      type: 'your_mocked_type',
+      id: 123,
+      username: 'your_mocked_username',
+      firstName: 'Your',
+      lastName: 'Name',
+      admin: true,
+    };
+
+    // Set form values
+    component.form.patchValue({
+      email: 'test@example.com',
+      password: 'password123',
+    });
+
+    const router = TestBed.inject(Router);
+
+    // Mock the navigate method to intercept without triggering actual navigation
+    const routerNavigateSpy = jest.spyOn(router, 'navigate');
+
+    const authServiceLoginSpy = jest
+      .spyOn(component['authService'], 'login')
+      .mockReturnValue(of(mockSessionInformation));
+    const sessionServiceLogIn = jest.spyOn(
+      component['sessionService'],
+      'logIn'
+    );
+
+    component.submit();
+    tick();
+    fixture.detectChanges();
+    flush();
+
+    // Check if the navigate function was called with the correct parameters
+    expect(routerNavigateSpy).toHaveBeenCalledWith(
+      ['/sessions'],
+      expect.anything()
+    );
+
+    // Ensure that navigate was called only once
+    expect(routerNavigateSpy).toHaveBeenCalledTimes(1);
+
+    // Assert other expectations
+    expect(authServiceLoginSpy).toHaveBeenCalledWith({
+      email: 'test@example.com',
+      password: 'password123',
+    });
+    expect(sessionServiceLogIn).toHaveBeenCalledWith(mockSessionInformation);
+
+    // Clear the mocks
+    authServiceLoginSpy.mockRestore();
+    sessionServiceLogIn.mockRestore();
+    routerNavigateSpy.mockRestore();
+  }));
 });
