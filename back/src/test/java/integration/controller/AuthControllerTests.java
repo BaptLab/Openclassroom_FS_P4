a93e5ea4526b0e98ae2daecd7f75bb9d968e0a6c
Baptiste LABAUNE <baptiste.labaune@gmail.com>
2024-01-16 17:32:45 +0100

    first integration test on controller is a success :)

--- back/src/test/java/integration/controller/AuthControllerTests.java
+++ back/src/test/java/integration/controller/AuthControllerTests.java
@@ -1,45 +1,42 @@
 package integration.controller;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
-import javax.servlet.ServletContext;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.openclassrooms.starterjwt.security.WebSecurityConfig;
+import com.openclassrooms.starterjwt.SpringBootSecurityJwtApplication;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
+import com.openclassrooms.starterjwt.payload.request.SignupRequest;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringBootSecurityJwtApplication.class)
 @AutoConfigureMockMvc
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { WebSecurityConfig.class })
-@WebAppConfiguration
 public class AuthControllerTests {
-	
 
-	private MockMvc mockMvc;
-	
-	
-	@Test
-	public void givenHomePageURI_whenMockMVC_thenReturnsIndexJSPViewName  () throws Exception{
-	    this.mockMvc.perform(get("/homePage")).andDo(print())
-	      .andExpect(view().name("index"));
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testLoginWithValidCredentials() throws Exception {
+        
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("yoga@studio.com");
+        loginRequest.setPassword("test!1234");
+        String jsonRequest = objectMapper.writeValueAsString(loginRequest);
+
+        // Perform the login request
+        mockMvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest))
+                .andExpect(status().isOk());
+    }
+
 }
